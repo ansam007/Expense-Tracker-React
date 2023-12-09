e58f2a1a8c0aab6663160f88fc44de44cf4fca2d
ansam007 <anshul.sharma5271@gmail.com>
2023-12-09 21:21:41 +0530

    add a location of expenditure tag and change the styling

--- src/components/expense.js
+++ src/components/expense.js
@@ -4,12 +4,14 @@ function Expenses() {
     const expenseItem = 'Car Insurance';
     const expenseDate = new Date();
     const expenseCost = 30000;
+    const expenseLocation = "Bangalore";
 
     return (
         <div className="expense-item">
             <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
             <h2>{expenseItem}</h2>
+            <div>{expenseLocation}</div>
                 <div className="expense-item__price">Rs.{expenseCost}</div>
             </div>   
         </div>
