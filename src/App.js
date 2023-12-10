c5d1a89045e2ce3aad1e2fb84ab68a512b1cbaba
ansam007 <anshul.sharma5271@gmail.com>
2023-12-10 20:53:06 +0530

    CORRECT-ED

--- src/App.js
+++ src/App.js
@@ -2,36 +2,37 @@ import './App.css';
 import Expenses from './components/expense';
 
 function App() {
+  
   const expenses = [
     {
       item: 'Groceries',
       cost: 50.00,
       place: 'Local Supermarket',
-      date: '2023-01-05'
+      date: new Date(2023,1,5),
     },
     {
       item: 'Dinner',
       cost: 30.00,
       place: 'Restaurant XYZ',
-      date: '2023-02-12'
+      date: new Date(2023,2,12),
     },
     {
       item: 'Gasoline',
       cost: 40.00,
       place: 'Gas Station ABC',
-      date: '2023-03-20'
+      date: new Date(2023,3,20),
     },
     {
       item: 'Movie Tickets',
       cost: 25.00,
       place: 'Cinema City',
-      date: '2023-04-15'
+      date: new Date(2023,4,15),
     },
     {
       item: 'Clothing',
       cost: 60.00,
       place: 'Fashion Store',
-      date: '2023-05-02'
+      date: new Date(2023,5,2),
     }
   ];
 
--- src/components/expense.js
+++ src/components/expense.js
@@ -1,12 +1,18 @@
 import "./Expense.css";
 
 function Expenses(props) {
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{props.date}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div>{props.place}</div>
       </div>
       <div className="expense-item__price">Rs.{props.amount}</div>
     </div>
