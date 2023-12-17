a6fda2f90dbf00e705c13aad024b4a34f4fc1361
ansam007 <anshul.sharma5271@gmail.com>
2023-12-17 17:14:43 +0530

    showing data in the console

--- src/components/userData/js/ExpenseForm.js
+++ src/components/userData/js/ExpenseForm.js
@@ -1,20 +1,31 @@
 import "../css/ExpenseForm.css";
 
 const ExpenseForm = () => {
+
+    function titleChanged (event){
+        console.log(event.target.value);
+    }
+    function amountChanged (event){
+        console.log(event.target.value);
+    }
+    function dateChanged (event){
+        console.log(event.target.value);
+    }
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text"></input>
+          <input type="text" onChange={titleChanged}></input>
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number"></input>
+          <input type="number" onChange={amountChanged}></input>
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date"></input>
+          <input type="date" onChange={dateChanged}></input>
         </div>
       </div>
       <div className="new-expense__actions">
