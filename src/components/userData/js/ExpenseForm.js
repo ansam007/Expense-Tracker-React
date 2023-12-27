ef7e81fa3288a280938284a5a037fe9af2c31eef
ansam007 <anshul.sharma5271@gmail.com>
2023-12-27 10:22:45 +0530

    changed the UI, now a button ADD NEW EXPENSE is shown on the screen and when you click on that button you can enter your add

--- src/components/userData/js/ExpenseForm.js
+++ src/components/userData/js/ExpenseForm.js
@@ -10,7 +10,7 @@ const ExpenseForm = (props) => {
     function titleChanged (event){
       setTitle(event.target.value);
     }
-    function amountChanged (event){
+    function amountChanged (event){ 
       setAmount(event.target.value);
     }
     function dateChanged (event){
@@ -50,6 +50,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type='button' onClick={props.onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
--- src/components/userData/js/UserExpense.js
+++ src/components/userData/js/UserExpense.js
@@ -1,18 +1,31 @@
 import "../css/UserExpense.css";
 import ExpenseForm from "./ExpenseForm";
+import {useState} from 'react';
 
 const UserData = (props) => {
+
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredData) => {
     const entereData = {
       ...enteredData
     }
-
     props.addExpense(entereData);
-    
+    setIsEditing(false);
   }
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  }
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  }
+
   return (
     <div className="new-expense">
-      <ExpenseForm saveExpenseData={saveExpenseDataHandler}></ExpenseForm>
+      {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+      {isEditing && <ExpenseForm saveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}></ExpenseForm>}
     </div>
   );
 };
